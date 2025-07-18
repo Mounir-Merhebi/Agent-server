ba6eb6e3dd5d45d29e224ee44cdd0ccaca20b904
MounirPrograms <mounirmerhebi21@gmail.com>
2025-07-18 23:26:27 +0300

    added the points i've went through

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,15 +1,10 @@
 <?php
 
 namespace Database\Seeders;
-
-// Removed: use App\Models\User; // No longer needed if you don't have a users table
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         $this->call(AgentSeeder::class);
